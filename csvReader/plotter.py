eaed510237772f998742399601ccde958a4d8039
gerhardmalberg88 <59392272+gerhardmalberg88@users.noreply.github.com>
2022-01-28 15:49:05 +0200

    Update plotter.py

--- csvReader/plotter.py
+++ csvReader/plotter.py
@@ -1,33 +1,70 @@
-'''
 import csv
-datafile = open('BatteryTestOutput-1726-time-edited.csv', 'r')
-#datareader = csv.reader(datafile, delimiter=';')
-data = list(csv.reader(open(datafile)))
-print(data)
-'''
-import csv
-dataFile = open('BatteryTestOutput-1726-time-edited.csv', 'r')
-dataReader = csv.reader(dataFile, delimiter=';')
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+
+dataFile = open("BatteryTestOutput-1726-time-edited.csv", "r") # BatteryTestOutput-1726-time-edited.csv
+dataReader = csv.reader(dataFile, delimiter=";")
 data = []
 for row in dataReader:
     data.append(row)
-dataRows = len(data)
-dataColumns = len(data[0])   
-print("Rows ", dataRows, " Columns ", dataColumns)
-print(type(data[1][0]))
-print(data[1][0])
-print("Converting to float")
-for row in range(1,6):
-    for col in range(0,6):
-        print(data[row][col])
-        ''' #Uncomment this section if unconvertability must be detected
+dataRowsSize = len(data)
+dataColumnsSize = len(data[0])   
+print("Rows ", dataRowsSize, " Columns ", dataColumnsSize)
+#print(type(data[1][0]))
+#print(data[1][0])
+#print("Converting to float")
+dataFloat = [[None for x in range(dataColumnsSize)] for y in range(dataRowsSize)] 
+for row in range(1,dataRowsSize):
+    for col in range(0,dataColumnsSize):
+        #print(data[row][col])      
         try:
-            float(element[row][col])
+            dataFloat[row][col] = float(data[row][col].replace(',', '.'))
         except ValueError:
-            print "Not a float"
-        '''
-        dataFloat = float(data[row][col].replace(',', '.'))
-        print(type(dataFloat))
-        print(dataFloat)
+            #print("Not a float")
+            isPatternTime = re.search(r"\d{2}.\d{2}.\d{2}", data[row][col])
+            if isPatternTime == None:
+                print("Bad luck, time pattern is invalid, enter time in following pattern as interger value <1> or time <23.59.59>")
+                exit()
+            else:
+                print("Time stamp at: ", isPatternTime.string)
+                #print(type(isPatternTime))
 
+#print("After Conversion ", dataFloat)
+numpyArrayDataFloat = np.array(dataFloat)
+transposeNumpyArrayDataFloat = numpyArrayDataFloat.T
+transposeToList = transposeNumpyArrayDataFloat.tolist()
+
+xAxis = transposeToList[0]
+yAxis = transposeToList 
+
+plt.scatter(xAxis, yAxis[0])
+plt.show()
+#exit()
+
+print("************")
+print(xAxis)
+print("************")
+print(yAxis[0])
+print("************")
+
+plt.plot(xAxis, yAxis[0], label = "line 1")
+ 
+plt.xlabel('x - axis')
+
+plt.ylabel('y - axis')
+
+plt.title('Two lines on same graph!')
+ 
+plt.legend()
+ 
+
+plt.show()
+
+# save as picture
+
+# First version with normal time stamp
+# Second version with original clock time stamp
+# arg parser for csv filename, x, and y data
+# headers must be present
 

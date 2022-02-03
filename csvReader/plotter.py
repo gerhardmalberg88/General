196c165158d9aa12427e499525a38c0881e93ede
gerhardmalberg88 <59392272+gerhardmalberg88@users.noreply.github.com>
2022-02-03 18:42:53 +0200

    Update plotter.py

--- csvReader/plotter.py
+++ csvReader/plotter.py
@@ -9,10 +9,10 @@ import argparse
 #Instantiate the parser
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--inputfilename", type=str, default="", required = True, 
+parser.add_argument("--inputfilename", type=str, default="BatteryTestOutput-1726-time-edited.csv", # TODO required = True, 
         help="File full name must be entered. For example: exampleinput.csv")
 
-parser.add_argument("--outputfilename", type=str, default="example", #required = True, 
+parser.add_argument("--outputfilename", type=str, default="", # TODO required = True, 
         help="File name must be entered. For example: exampleoutput")
 
 parser.add_argument("--xaxis", type=str, default="Time",  required = True,
@@ -32,7 +32,7 @@ parser.add_argument("--title", type=str, default="title",
 
 args = parser.parse_args()
 
-dataFile = open("BatteryTestOutput-1726-time-edited.csv", "r") # BatteryTestOutput-1726-time-edited.csv
+dataFile = open(args.inputfilename, "r") # BatteryTestOutput-1726-time-edited.csv
 dataReader = csv.reader(dataFile, delimiter=";")
 data = []
 for row in dataReader:
@@ -51,23 +51,30 @@ for col in range(0,dataColumnsSize):
         print("Enter headers in correct alphabetic format ", dataHeaderStripped)
         exit()
 
-# TODO Check if xaxis header is valid
+# Check if xaxis header is valid
+if args.yaxiscount >= 1:
+    columnsToPlot = [None for x in range(args.yaxiscount+1)]
+if args.yaxiscount == 0:
+    columnsToPlot = [None for x in range(dataColumnsSize+1)]
+
 headerNameCounter = 0
 prevHeaderNameCounter = 0
 for i in range(0, 1):
     for col in range(0,dataColumnsSize):
         if args.xaxis == data[0][col]:
             headerNameCounter = headerNameCounter + 1
+            columnsToPlot[0] = col
     if headerNameCounter == prevHeaderNameCounter:
         while prevHeaderNameCounter == headerNameCounter:
             print("Following header is not found in file: ", args.xaxis, end=". ")
             args.xaxis = input("Enter correct header name: ")
-            for j in range(0,dataColumnsSize):
-                if args.xaxis == data[0][j]:
+            for col in range(0,dataColumnsSize):
+                if args.xaxis == data[0][col]:
                     headerNameCounter = headerNameCounter + 1
+                    columnsToPlot[0] = col
     prevHeaderNameCounter = headerNameCounter
 
-# Get header names if yaxiscount is more than, else plot all
+# Get header names if yaxiscount is more than 0, else plot all
 if args.yaxiscount >= 1:
     yAxisHeaderNames = [None for x in range(args.yaxiscount)]
     for i in range(0,args.yaxiscount):
@@ -75,23 +82,33 @@ if args.yaxiscount >= 1:
         yAxisHeaderNames[i] = input()
     #print(yAxisHeaderNames)
 
-# Headers match and index
-headerNameCounter = 0
-prevHeaderNameCounter = 0
-for i in range(0, len(yAxisHeaderNames)):
-    for col in range(0,dataColumnsSize):
-        if yAxisHeaderNames[i] == data[0][col]:
-            headerNameCounter = headerNameCounter + 1
-    if headerNameCounter == prevHeaderNameCounter:
-        while prevHeaderNameCounter == headerNameCounter:
-            print("Following header is not found in file: ", yAxisHeaderNames[i], end=". ")
-            yAxisHeaderNames[i] = input("Enter correct header name: ")
-            for j in range(0,dataColumnsSize):
-                if yAxisHeaderNames[i] == data[0][j]:
-                    headerNameCounter = headerNameCounter + 1
-    prevHeaderNameCounter = headerNameCounter 
+    # Headers match and index
+    headerNameCounter = 0
+    prevHeaderNameCounter = 0
+    for i in range(0, len(yAxisHeaderNames)):
+        for col in range(0,dataColumnsSize):
+            if yAxisHeaderNames[i] == data[0][col]:
+                headerNameCounter = headerNameCounter + 1
+                columnsToPlot[i+1] = col
+        if headerNameCounter == prevHeaderNameCounter:
+            while prevHeaderNameCounter == headerNameCounter:
+                print("Following header is not found in file: ", yAxisHeaderNames[i], end=". ")
+                yAxisHeaderNames[i] = input("Enter correct header name: ")
+                for col in range(0,dataColumnsSize):
+                    if yAxisHeaderNames[i] == data[0][col]:
+                        headerNameCounter = headerNameCounter + 1
+                        columnsToPlot[i+1] = col
+        prevHeaderNameCounter = headerNameCounter 
 
-exit()
+# If yaxiscount = 0 then plot all columns
+if args.yaxiscount == 0:
+    for col in range(0, dataColumnsSize):
+        if args.xaxis != data[0][col]:
+            columnsToPlot[col+1] = col
+    #print(columnsToPlot)
+    removeNones = [i for i in columnsToPlot if i != None]
+    columnsToPlot = removeNones
+
 
 # Dataconversion
 dataFloat = [[None for x in range(dataColumnsSize)] for y in range(dataRowsSize)] 
@@ -122,21 +139,24 @@ yAxis = transposedToList
 #plt.show()
 #exit()
 
+'''
 print("************")
 print(xAxis)
 print("************")
 print(yAxis[3])
 print("************")
+'''
 
-plt.plot(xAxis, yAxis[3], label = "line 1") # TODO Forloop these
-plt.plot(xAxis, yAxis[2], label = "line 2") # TODO Forloop these
+for i in range(1, len(columnsToPlot)):
+    plt.plot(xAxis, yAxis[columnsToPlot[i]], label = data[0][columnsToPlot[i]])
 
-plt.xlabel('x - axis')
-plt.ylabel('y - axis')
-plt.title('Two lines on same graph!')
+plt.xlabel(args.xlabel)
+plt.ylabel(args.ylabel)
+plt.title(args.title)
 plt.legend()
-#plt.show()
+plt.show()
 
-# arg parser for csv filename, x from header, y from header, y2 from header, y3 from header, title define
-# add plot all headers
+
 # save as picture
+# calculate max avg min values
+# add support for white spaced argument
